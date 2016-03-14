f23c2fa6b443fcb97740e7dbe147ea893cb51db7
mYmd <m-yamada@f06.itscom.net>
2016-03-14 21:01:24 +0900

    Create odbcResource.hpp

---
+++ odbcResource.hpp
@@ -0,0 +1,113 @@
+//odbcResource.hpp
+//Copyright (c) 2015 mmYYmmdd
+
+#pragma once
+
+#include <sql.h>
+#include <sqlext.h>
+#include <odbcinst.h>
+#include <tchar.h>
+#include <string>
+#include <vector>
+#include <array>
+
+#pragma comment(lib, "odbccp32.lib")
+
+//http://www.happy2-island.com/access/gogo03/capter00416.shtml
+
+//**************************************************************
+class odbc_raii_env	{
+	HENV	henv;
+	odbc_raii_env(const odbc_raii_env&) = delete;
+	odbc_raii_env(odbc_raii_env&&) = delete;
+	odbc_raii_env& operator =(const odbc_raii_env&) = delete;
+	odbc_raii_env& operator =(odbc_raii_env&&) = delete;
+public:
+	odbc_raii_env();
+	~odbc_raii_env();
+	void AllocHandle();
+	template <typename T>
+	RETCODE invoke(T&& expr) const
+	{	return (std::forward<T>(expr))(henv);	}
+};
+
+//**************************************************************
+class odbc_raii_connect	{
+	HDBC	hdbc;
+	odbc_raii_connect(const odbc_raii_connect&) = delete;
+	odbc_raii_connect(odbc_raii_connect&&) = delete;
+	odbc_raii_connect& operator =(const odbc_raii_connect&) = delete;
+	odbc_raii_connect& operator =(odbc_raii_connect&&) = delete;
+public:
+	odbc_raii_connect();
+	~odbc_raii_connect();
+	void AllocHandle(const odbc_raii_env& env);
+	template <typename T>
+	RETCODE invoke(T&& expr) const
+	{	return (std::forward<T>(expr))(hdbc);	}
+};
+
+//**************************************************************
+class odbc_raii_statement	{
+	HSTMT	hstmt;
+	odbc_raii_statement(const odbc_raii_statement&) = delete;
+	odbc_raii_statement(odbc_raii_statement&&) = delete;
+	odbc_raii_statement& operator =(const odbc_raii_statement&) = delete;
+	odbc_raii_statement& operator =(odbc_raii_statement&&) = delete;
+public:
+	using tstring = std::basic_string <TCHAR> ;
+	odbc_raii_statement();
+	~odbc_raii_statement();
+	tstring AllocHandle(const tstring& connectName, const odbc_raii_connect& con);
+	template <typename T>
+	RETCODE invoke(T&& expr) const
+	{	return (std::forward<T>(expr))(hstmt);	}
+};
+
+//**************************************************************
+class cursor_colser	{
+	const odbc_raii_statement&	h_;
+public:
+	cursor_colser(const odbc_raii_statement& h);
+	~cursor_colser();
+};
+
+//**************************************************************
+class odbc_raii_select	{
+	odbc_raii_select(const odbc_raii_select&) = delete;
+	odbc_raii_select(odbc_raii_select&&) = delete;
+	odbc_raii_select& operator =(const odbc_raii_select&) = delete;
+	odbc_raii_select& operator =(odbc_raii_select&&) = delete;
+public:
+	using tstring = odbc_raii_statement::tstring;
+	using result_type = std::vector<std::vector<tstring>>;
+    static const std::size_t    StrSizeofColumn = 512;
+    static const std::size_t    ColumnNameLen = 256;
+    using column_name_type = std::array<TCHAR, ColumnNameLen>;
+    odbc_raii_select()  {}
+    ~odbc_raii_select() {}
+    RETCODE execDirect(const tstring&, const odbc_raii_statement&) const;
+    SQLSMALLINT columnAttribute(const tstring&                  ,
+                                const odbc_raii_statement&      ,
+                                std::vector<column_name_type>&  ,
+                                std::vector<SQLSMALLINT>&       ,
+                                std::vector<SQLULEN>&           ,
+                                std::vector<SQLSMALLINT>&       ,
+                                std::vector<SQLSMALLINT>&       ,
+                                std::vector<SQLSMALLINT>&       ,
+                                std::vector<SQLLEN>&            ,
+                                std::vector<std::basic_string<UCHAR>>*       
+                                ) const;
+    result_type select( int                             timeOutSec,
+                        const tstring&                  sql_expr,
+                        const odbc_raii_statement&      stmt,
+                        std::vector<column_name_type>*  pColname,
+                        std::vector<SQLSMALLINT>*       pColnamelen,
+                        std::vector<SQLULEN>*           pCollen,
+                        std::vector<SQLSMALLINT>*       pNullable,
+                        std::vector<SQLSMALLINT>*       pColtype,
+                        std::vector<SQLSMALLINT>*       pScale,
+                        std::vector<SQLLEN>*            pDatastrlen
+                        ) const;
+};
+
