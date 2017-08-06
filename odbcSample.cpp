cc23841ed4a672460aa5cb7680ba846ddcbe06cd
mYmd <m-yamada@f06.itscom.net>
2017-08-06 19:11:07 +0900

    Cursor_type指定の例

--- odbcSample.cpp
+++ odbcSample.cpp
@@ -25,10 +25,45 @@ int main()
         elem.resize(col_N);
     };
     auto recordLen = select_table(o_o.stmt(),
-        tstring{ _T("SELECT * FROM myTable") },
-        no_header{},
-        init_func,
-        elem_func,
-        add_func);
+                            tstring{ _T("SELECT * FROM myTable") },
+                        no_header{},
+                    init_func,
+                elem_func,
+            add_func);
+    return 0;
+}
+
+int main_2()
+{
+    odbc_set o_o{tstring{
+        _T("Driver={SQL Server Native Client 11.0}; Trusted_Connection=YES; Server=MY-PC\\SQLEXPRESS; DATABASE=sampleDB;")
+        }
+        ,
+        SQL_CURSOR_STATIC
+    };
+    std::vector<std::vector<tstring>> vec;
+    std::vector<tstring> elem;
+    SQLSMALLINT col_N{ 0 };
+    auto init_func = [&](SQLSMALLINT c) {
+        elem.resize(col_N = c);
+    };
+    auto elem_func = [&](SQLSMALLINT j, TCHAR const* str, SQLSMALLINT coltype) {
+        if (str)    elem[j] = str;
+        else        elem[j] = _T("NULL");
+    };
+    auto add_func = [&](std::size_t x) {
+        vec.push_back(std::move(elem));
+        elem.resize(col_N);
+    };
+    auto Fetch_expr = [&](HSTMT x) {
+        return ::SQLFetchScroll(x, SQL_FETCH_RELATIVE, 3*1024);
+    } ;
+    auto recordLen = select_table(o_o.stmt(),
+                                tstring{ _T("SELECT * FROM myTable") },
+                            no_header{},
+                        init_func,
+                    elem_func,
+                add_func,
+            Fetch_expr);
     return 0;
 }
